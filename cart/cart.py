e9e1d4f7c396a4c5664eb62c7efab44278e54dd7
Balamurugan-Testpress <balamurugan@testpress.in>
2025-07-02 11:25:36 +0530

    add cart quantity update and cart count

--- cart/cart.py
+++ cart/cart.py
@@ -5,20 +5,13 @@ from shop.models import Product
 
 class Cart(object):
     def __init__(self, request):
-        """
-        Initialize the cart.
-        """
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
-            # save an empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
     def add(self, product, quantity=1, override_quantity=False):
-        """
-        Add a product to the cart or update its quantity.
-        """
         product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] = {"quantity": 0, "price": str(product.price)}
@@ -26,7 +19,7 @@ class Cart(object):
             self.cart[product_id]["quantity"] = quantity
         else:
             self.cart[product_id]["quantity"] += quantity
-            self.save()
+        self.save()
 
     def save(self):
         # mark the session as "modified" to make sure it gets saved
